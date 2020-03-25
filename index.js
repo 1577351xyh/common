5300d502b8a429cddcdace9288f6f9dd16175fc5
1577351xyh <564146958@qq.com>
2020-03-25 23:19:54 +0800

    clone function

--- index.js
+++ index.js
@@ -151,3 +151,23 @@ function merge(dest, src) {
     }
   }
 }
+
+function clone(obj) {
+  let obj2;
+  if (typeof obj == 'object') {
+    if (obj instanceof Array) {
+      obj2 = []
+      for (let i = 0; i < obj.length; i++) {
+        obj2[i] = clone(obj[i])
+      }
+    } else {
+      obj2 = {}
+      for (let key in obj) {
+        obj2[key] = clone(obj[key])
+      }
+    }
+    return obj2;
+  } else {
+    return obj
+  }
+}
